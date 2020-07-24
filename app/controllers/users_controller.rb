4fab7f26eed9c53cea543bf611c7b4027c653048
Huong Tra <nguyen.thi.huong.tra@sun-asterisk.com>
2020-07-24 14:49:34 +0700

    chapter7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by params[:id]
+    return if @user
+
+    flash[:danger] = t "users.new.flash"
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "users.new.title"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit User::USERS_PARAMS
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = Settings.validations.user.email.regex
+  USERS_PARAMS = %i(name email password password_confirmation).freeze
 
-  validates :name, presence: true, 
+  validates :name, presence: true,
     length: {maximum: Settings.validations.user.name.max_length}
-  validates :email, presence: true, 
+  validates :email, presence: true,
     length: {maximum: Settings.validations.user.email.max_length},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: true
-  validates :password, presence: true, 
+  validates :password, presence: true,
     length: {minimum: Settings.validations.user.password.min_length}
 
   before_save :downcase_email
@@ -16,7 +17,7 @@ class User < ApplicationRecord
 
   private
 
-    def downcase_email
-      email.downcase!
-    end
+  def downcase_email
+    email.downcase!
+  end
 end
