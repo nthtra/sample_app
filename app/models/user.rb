35f32bf7b2eb7e59c6e5b6adfb93a26590b53a87
Huong Tra <nguyen.thi.huong.tra@sun-asterisk.com>
2020-08-08 18:29:59 +0700

    chapter12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.validations.user.email.regex
   USERS_PARAMS = %i(name email password password_confirmation).freeze
-  attr_accessor :remember_token, :activation_token
+  RESET_PASSWORD_PARAMS = %i(password password_confirmation).freeze
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true,
@@ -54,6 +55,19 @@ class User < ApplicationRecord
 
   def activate
     update activated: true, activated_at: Time.zone.now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.time.expired.hours.ago
   end
 
   private
